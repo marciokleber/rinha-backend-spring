13937b969d8d6cb2736352fa00e31e83a4d90e8c
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2025-01-01 11:49:47 -0300

    :tada: First commit -  criação do projeto para rinha de backend 2023 usando spring

---
+++ src/main/java/com/github/marciokleber/rinhabackendspring/RinhaBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.rinhabackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinhaBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RinhaBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/marciokleber/rinhabackendspring/RinhaBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.rinhabackendspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinhaBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
